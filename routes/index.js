144a8f0b64779bff159ac95ae28264ca5376b23a
ageapps <age.apps.android@gmail.com>
2016-04-26 17:33:59 +0000

    primera pagina y favicon

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz'
+  , subtitle:'El portal donde podrá crear sus propios juegos!'});
+//  res.render('index', { pepe:'El portal donde podrá crear sus propios juegos!' });
+//res.render('index', { subtitle:'El portal donde podrá crear sus propios juegos!'});
 });
 
 module.exports = router;
