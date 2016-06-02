257dc952d4b05eaa5cc2a7c8c16ab80d990b522c
ageapps <age.apps.android@gmail.com>
2016-06-03 01:27:22 +0200

    Description and Category Icons, More design... Upload Heroku

--- migrations/20160520120024-CreateQuizzesTable.js
+++ migrations/20160520120024-CreateQuizzesTable.js
@@ -2,14 +2,6 @@
 
 module.exports = {
     up: function(queryInterface, Sequelize) {
-        /*
-          Add altering commands here.
-          Return a promise to correctly handle asynchronicity.
-heroku run ./node_modules/.bin/sequelize db:seed:all --url postgres://hcarthtolgfcud:z3ptZMwk3mFW7wz0JSxW6k2TEm@ec2-23-23-224-174.compute-1.amazonaws.com:5432/dcgefj7svtar15
-          Example:
-          ./node_modules/.bin/sequelize migration:create --name AddCategoriesIdToQuizzesTable --url sqlite://$(pwd)/quiz.sqlite
-          return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-        */
         return queryInterface.createTable('Quizzes', {
             id: {
                 type: Sequelize.INTEGER,
